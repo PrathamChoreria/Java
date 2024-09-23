ce95f986d6eb17a2c75f02717eeb78fdb90423e8
PRATS CHORERIA <prathamchoreria@gmail.com>
2024-09-23 10:31:14 +0530

    changes

--- src/com/deloitte/lab041/ex1/Exercise1.java
+++
@@ -1,5 +0,0 @@
-package com.deloitte.lab041.ex1;
-
-public class Exercise1 {
-
-}
--- src/com/deloitte/lab41/ex02/Ex02.java
+++
@@ -1,9 +0,0 @@
-package com.deloitte.lab04.ex02;
-
-public class Ex02 {
-	
-
-	//All in packages inside com.cg.eis
-
-
-}
